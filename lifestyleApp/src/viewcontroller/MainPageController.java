09fa0cf136c11b1e73db73a4d19e34317a1e55f4
johannawiberg <johanna440@live.se>
2020-04-22 00:31:46 +0200

    Nu går det att byta page till Statistik sidan & Kalendersidan men inte med hjälp av page loadern.

--- lifestyleApp/src/viewcontroller/MainPageController.java
+++ lifestyleApp/src/viewcontroller/MainPageController.java
@@ -8,34 +8,91 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import model.MainModel;
-
 import java.util.Optional;
 
 public class MainPageController {
+
 
     @FXML
     private Button headerButtonCalendar;
 
     @FXML
-    private ImageView HeaderImage1;
-
-    @FXML
     private Button headerButtonStatistics;
 
+    @FXML
+    private Button headerButtonWorkout;
 
+    @FXML
+    private ImageView HeaderImage5;
+
+    @FXML
+    private Button headerButtonSettings;
+
+    @FXML
+    private ImageView HeaderImage6;
+
+    @FXML
+    private Button headerButtonYesterday;
+
+    @FXML
+    private ImageView HeaderImage3;
+
+    @FXML
+    private Button headerButtonTomorrow;
+
+    @FXML
+    private ImageView HeaderImage4;
+
+    @FXML
+    private TextField HeaderTextFieldDate;
+
+    @FXML
+    private Text headerName;
+
+    @FXML
+    private AnchorPane mainpageAnchorpane;
+
+    @FXML
+    private AnchorPane calendarAnchorPane;
+
+    @FXML
+    private AnchorPane statisticsAnchorPane;
+
+
+    @FXML
+    void showCalendarPage(ActionEvent event) {
+    calendarAnchorPane.toFront();
+    System.out.print("Calendar");
+    }
+
+    @FXML
+    void showStatisticsPage(ActionEvent event) {
+        statisticsAnchorPane.toFront();
+        System.out.print("statistics");
+    }
+
+    public void initPage(MainModel model, Optional<Object> empty) {
+    }
+
+
+
+
+
+
+
+
+/*
     private MainModel model;
-
-
     private AnchorPane homePage;
     private AnchorPane calendarPage;
     private AnchorPane statisticsPage;
 
 
     public void initPage(MainModel model, Optional<MainPageController> mainPage) {
-       /* this.model = model;
+       *//* this.model = model;
          homePage = (AnchorPane) PageLoader.createMainPage(this);
         calendarPage = PageLoader.createCalendarPage(this);
-        statisticsPage = PageLoader.createStatisticsPage(this);*/
+        statisticsPage = PageLoader.createStatisticsPage(this);*//*
 
     }
 
@@ -58,7 +115,7 @@ public class MainPageController {
         homePage.getChildren().clear();
         homePage.getChildren().add(pane);
         homePage.toFront();
-    }
+    }*/
 
 
   /*  @FXML
