1d0db3e2b11da6e79a85bc1a2e4f3bb7e5703762
oscarforsberg <oscarforsbergg@hotmail.com>
2020-04-21 20:05:24 +0200

    Created some buttons for the header.

--- lifestyleApp/src/viewcontroller/MainPageController.java
+++ lifestyleApp/src/viewcontroller/MainPageController.java
@@ -1,10 +1,45 @@
 package viewcontroller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import model.MainModel;
 
+
+import javafx.scene.image.ImageView;
 import java.util.Optional;
 
 public class MainPageController {
     public void initPage(MainModel model, Optional<Object> empty) {
     }
+    @FXML
+    private Button HeaderButton1;
+    @FXML
+    private Button HeaderButton2;
+    @FXML
+    private Button HeaderButton3;
+    @FXML
+    private Button HeaderButton4;
+    @FXML
+    private Button HeaderButton5;
+    @FXML
+    private Button HeaderButton6;
+    @FXML
+    private ImageView HeaderImage1;
+    @FXML
+    private ImageView HeaderImage2;
+    @FXML
+    private ImageView HeaderImage3;
+    @FXML
+    private ImageView HeaderImage4;
+    @FXML
+    private ImageView HeaderImage5;
+    @FXML
+    private ImageView HeaderImage6;
+    @FXML
+    private TextField HeaderTextFieldName;
+    @FXML
+    private TextField HeaderTextFieldDate;
+
+
 }
