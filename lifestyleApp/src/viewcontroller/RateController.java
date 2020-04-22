c89720f26ca8228a2f1c726715b2abb6e7eb36c5
Hannat98 <hannatarnasen@hotmail.com>
2020-04-22 11:49:38 +0200

    Fixed buttons completely

--- lifestyleApp/src/viewcontroller/RateController.java
+++ lifestyleApp/src/viewcontroller/RateController.java
@@ -38,29 +38,82 @@ public class RateController {
 
     @FXML
     void rate1star(MouseEvent event) {
-        if (star1anchor.getBlendMode() == BlendMode.SRC_OVER && star2anchor.getBlendMode() == BlendMode.SRC_OVER)
+        if (star1anchor.getBlendMode() == BlendMode.SRC_OVER && star2anchor.getBlendMode() == BlendMode.OVERLAY) {
+            resetStars();
+            re.setRating(0);
+        } else {
+            star1anchor.setBlendMode(BlendMode.SRC_OVER);
+            star2anchor.setBlendMode(BlendMode.OVERLAY);
+            star3anchor.setBlendMode(BlendMode.OVERLAY);
+            star4anchor.setBlendMode(BlendMode.OVERLAY);
+            star5anchor.setBlendMode(BlendMode.OVERLAY);
             re.setRating(1);
-        star1anchor.setBlendMode(BlendMode.SRC_OVER);
+        }
     }
 
     @FXML
     void rate2star(MouseEvent event) {
-
+        if (star2anchor.getBlendMode() == BlendMode.SRC_OVER && star3anchor.getBlendMode() == BlendMode.OVERLAY) {
+            resetStars();
+        } else {
+            star1anchor.setBlendMode(BlendMode.SRC_OVER);
+            star2anchor.setBlendMode(BlendMode.SRC_OVER);
+            star3anchor.setBlendMode(BlendMode.OVERLAY);
+            star4anchor.setBlendMode(BlendMode.OVERLAY);
+            star5anchor.setBlendMode(BlendMode.OVERLAY);
+            re.setRating(2);
+        }
     }
 
     @FXML
     void rate3star(MouseEvent event) {
-
+        if (star3anchor.getBlendMode() == BlendMode.SRC_OVER && star4anchor.getBlendMode() == BlendMode.OVERLAY) {
+            resetStars();
+        } else {
+            star1anchor.setBlendMode(BlendMode.SRC_OVER);
+            star2anchor.setBlendMode(BlendMode.SRC_OVER);
+            star3anchor.setBlendMode(BlendMode.SRC_OVER);
+            star4anchor.setBlendMode(BlendMode.OVERLAY);
+            star5anchor.setBlendMode(BlendMode.OVERLAY);
+            re.setRating(3);
+        }
     }
 
     @FXML
     void rate4star(MouseEvent event) {
-
+        if (star4anchor.getBlendMode() == BlendMode.SRC_OVER && star5anchor.getBlendMode() == BlendMode.OVERLAY) {
+            resetStars();
+        } else {
+            star1anchor.setBlendMode(BlendMode.SRC_OVER);
+            star2anchor.setBlendMode(BlendMode.SRC_OVER);
+            star3anchor.setBlendMode(BlendMode.SRC_OVER);
+            star4anchor.setBlendMode(BlendMode.SRC_OVER);
+            star5anchor.setBlendMode(BlendMode.OVERLAY);
+            re.setRating(4);
+        }
     }
 
     @FXML
     void rate5star(MouseEvent event) {
+        if (star5anchor.getBlendMode() == BlendMode.SRC_OVER) {
+            resetStars();
+        } else {
+            star1anchor.setBlendMode(BlendMode.SRC_OVER);
+            star2anchor.setBlendMode(BlendMode.SRC_OVER);
+            star3anchor.setBlendMode(BlendMode.SRC_OVER);
+            star4anchor.setBlendMode(BlendMode.SRC_OVER);
+            star5anchor.setBlendMode(BlendMode.SRC_OVER);
+            re.setRating(5);
+        }
+    }
 
+    private void resetStars(){
+        star1anchor.setBlendMode(BlendMode.OVERLAY);
+        star2anchor.setBlendMode(BlendMode.OVERLAY);
+        star3anchor.setBlendMode(BlendMode.OVERLAY);
+        star4anchor.setBlendMode(BlendMode.OVERLAY);
+        star5anchor.setBlendMode(BlendMode.OVERLAY);
+        re.setRating(0);
     }
 
     public void initPage(MainModel model, Optional<Object> empty) {
