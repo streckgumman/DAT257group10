6026ca03c1d3851c6b86b9072d56b3feb9bd3344
Linnea Johansson <43148111+streckgumman@users.noreply.github.com>
2020-05-07 10:50:50 +0200

    Merge branch 'training' of C:\Users\linne\OneDrive\Documents\GitHub\DAT257group10 with conflicts.

--- lifestyleApp/src/model/Todo.java
+++ lifestyleApp/src/model/Todo.java
@@ -1,4 +1,31 @@
 package model;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Todo {
+    private ArrayList<TodoEntry> todos = new ArrayList<>();
+
+    public void newEntry(String text, LocalDate date){
+        addEntry(new TodoEntry(text,date));
+    }
+
+    public void addEntry(TodoEntry entry) {
+        todos.add(entry);
+    }
+
+    public void deleteEntry(TodoEntry entry) {
+        todos.remove(entry);
+    }
+
+    public TodoEntry getCurrentEntry(LocalDate date) {
+
+        for (TodoEntry entry : todos) {
+            if (entry.getDate().equals(date)) {
+                return entry;
+            }
+        }
+        return null;
+    }
 }
